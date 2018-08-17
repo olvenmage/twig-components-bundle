2680453c14c7f9ff3ffe1cad684650530436376c
olveneer <34474220+olvenmage@users.noreply.github.com>
2018-08-17 17:30:22 +0200

    Add files via upload

---
+++ Resources/src/TwigComponentPass.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Olveneer\TwigComponentsBundle\Resources\src;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Class TwigComponentPass
+ * @package App\Olveneer\TwigComponentsBundle\Resources\src
+ */
+class TwigComponentPass implements CompilerPassInterface
+{
+
+    /**
+     * Registers all services with the 'olveneer.component' tag as valid components.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->has(TwigComponentStore::class)) {
+            return;
+        }
+
+        $definition = $container->findDefinition(TwigComponentStore::class);
+
+        $taggedServices = $container->findTaggedServiceIds('olveneer.component');
+
+        foreach ($taggedServices as $id => $tags) {
+            $definition->addMethodCall('addComponent', [new Reference($id)]);
+        }
+    }
+
+}
---
+++ Resources/src/TwigComponentStore.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Olveneer\TwigComponentsBundle\Resources\src;
+
+/**
+ * Class TwigComponentStore
+ * @package App\Olveneer\TwigComponentsBundle\Resources\src
+ */
+class TwigComponentStore
+{
+    /**
+     * @var TwigComponentInterface[]
+     */
+    private $components = [];
+
+
+    /**
+     * Adds a component to the store
+     *
+     * @param TwigComponentInterface|null $component
+     */
+    public function addComponent($component)
+    {
+        if (!$component instanceof TwigComponentInterface) {
+            return;
+        }
+
+        $this->components[$component->getAlias()] = $component;
+    }
+
+    /**
+     * Retrieves a component out of the store using it's alias
+     *
+     * @param $alias
+     * @return TwigComponentInterface|null
+     */
+    public function getComponent($alias): ?TwigComponentInterface
+    {
+        if (!isset($this->components[$alias])) {
+            return null;
+        }
+
+        return $this->components[$alias];
+    }
+}
